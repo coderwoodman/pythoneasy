b8545a3243708651e23c100beb2ff8d8272f4ead
coderwood <coderwood@hotmail.com>
2014-09-10 22:05:15 +0800

    init

---
+++ easypython.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+import MySQLdb
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+db = MySQLdb.connect(user='root', db='myblog', passwd='jacky983', host='114.215.154.9',charset='utf8')
+cursor = db.cursor()
+cursor.execute('SELECT blogtitle FROM blogs')
+for row in cursor.fetchall():
+    str=row[0] 
+    print str
+db.close()
+
---
+++ easysqllite.py
@@ -0,0 +1,21 @@
+﻿#coding=utf-8
+
+import sqlite3
+
+cx=sqlite3.connect("test.db")
+cu=cx.cursor()
+#cu.execute("create table catalog (id integer primary key,pid integer,name varchar(10) UNIQUE,nickname text NULL)")
+
+#for t in[(0,10,'abc','Yu'),(1,20,'cba','Xu')]:
+#    cx.execute("insert into catalog values (?,?,?,?)", t)
+#cx.commit()   
+
+x=u'张'
+cu.execute("update catalog set name=? where id=0",x)
+
+cu.execute("select * from catalog")
+for item in cu.fetchall():
+    for element in item:
+        print element,  
+    print
+
